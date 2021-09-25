b0570ec7c32a3d3c22d2b01a92e4c54030f83ee6
kemaldede <35036769+kemaldede@users.noreply.github.com>
2021-09-25 19:35:42 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,14 +7,8 @@ echo -e "\n## Installing prerequisites"
 sudo DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends tzdata
 
 case $esp_idf_version in
-  latest | v4.2 | v4.2.* | release/v4.2)
+  latest | v4.3 | v4.3.* | release/v4.3)
     sudo apt-get install -y git wget flex bison gperf python3 python3-pip python3-setuptools python-is-python3 python3-libusb1 cmake ninja-build ccache libffi-dev libssl-dev dfu-util
-    ;;
-  v4.1 | v4.1.* | release/v4.1)
-    sudo apt-get install -y git wget flex bison gperf python3 python3-pip python3-setuptools python-is-python3 python3-libusb1 cmake ninja-build ccache libffi-dev libssl-dev
-    ;;
-  v4.0.* | release/v4.0)
-    sudo apt-get install -y git wget libncurses-dev flex bison gperf python3 python3-pip python3-setuptools python-is-python3 python3-libusb1 cmake ninja-build ccache libffi-dev libssl-dev
     ;;
   *)
     echo "Unimplemented ESP-IDF version !"
